085746bbd17efc4e1174fbc21073739a2142ded3
coronelhernan <hcoronel1996@gmail.com>
2026-01-08 14:20:42 -0300

    updated hero section design and navbar

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,18 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
+import path from 'path';
 
-// https://astro.build/config
+
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+    resolve: {
+      alias: {
+        '@components': path.resolve('./src/components'),
+        '@layouts': path.resolve('./src/layouts'),
+        '@styles': path.resolve('./src/styles'),
+        '@assets': path.resolve('./src/assets'),
+      },
+    },
+  },
 });
