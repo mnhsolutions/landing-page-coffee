f96b5b1dc4dd5aba4f4e4b2311ac044ceb80ff1e
coronelhernan <hcoronel1996@gmail.com>
2026-02-02 16:48:44 -0300

    chore(netlify): configure Astro Netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
+import netlify from '@astrojs/netlify';
 import react from '@astrojs/react';
 import path from 'path';
 
 export default defineConfig({
+  output: "server",
+  adapter: netlify(), 
+
   integrations: [react()], 
   vite: {
     server: {
